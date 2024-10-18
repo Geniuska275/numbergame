a38a9327a25619b59798bf507cdedf1d3b124a25
Geniuska275 <aigbojie2020@gmail.com>
2024-10-18 12:20:13 +0100

    updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer() {
   return (
     <div className='footer bg-[#EDF2EA]'>
-        <div className='flex  Foot justify-evenly items center border-b p-2 '>
+        <div className='Foot border-b p-2 '>
             <div className='space'>
              <img src="" />
               <div>
@@ -12,13 +12,14 @@ export default function Footer() {
                 <br></br> off Akpakpava road,<br></br> Benin City, <br></br>Edo State.
                 </p>
               </div>
-              <div className='mt-2'>
+            
+            </div>
+            <div className='space mb-5'>
                 <h1>Warri Branch :</h1>
                 <p  className='text-sm text-[#4A4E55]'> 28, Effurum Sapele Road,<br></br> opposite unity bank,
                 <br></br> before Enerhen junction ,<br></br>warri Delta State.
                 </p>
               </div>
-            </div>
             <div className='space' >
                 <h1> Company</h1>
                 <a>
--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Statistics() {
   return (
-    <div className='bg-white flex  justify-around p-5 mb-[100px] items-center shadow-lg'>
+    <div className='Stats bg-white  p-5 mb-[100px]  shadow-lg'>
       <div className='flex gap-2 font-[lato] space '>
         <h1 className='font-bold text-3xl text-[#37CA2A]'> 5K+</h1>
         <p className='text-[#4A4E55]'>Happy <br></br>Clients</p>
