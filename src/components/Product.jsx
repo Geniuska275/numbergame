ce49f4965eacdf74ffbf254303103cc288a5a4e8
Geniuska275 <aigbojie2020@gmail.com>
2024-10-21 10:58:26 +0100

    updated

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -5,7 +5,7 @@ function Product({images, name, description}) {
     <div className='w-[300px] shadow-lg p-[20px] rounded'>
         <img src={images} className='w-full cover mt-2'/>
         <h1 className='font-extrabold font-[lato]  text-2xl mb-2'> {description}</h1>
-        <p className='font-bold font-[lato] text-[#4A4E55] text-sm'>{name}</p>
+        <p className='font-bold font-[lato] text-[#4A4E55] text-sm mb-2'>{name}</p>
          
          <button className="bg-[#37CA2A]
           md: rounded-md w-full
