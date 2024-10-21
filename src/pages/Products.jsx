abed187472e43c8c6136922df2568ec6fc7fc267
Geniuska275 <aigbojie2020@gmail.com>
2024-10-21 11:07:09 +0100

    updated

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -6,26 +6,26 @@ export default function Products() {
 
   const [products,setProducts]=useState([{
     images:"https://st4.depositphotos.com/11085122/21452/i/450/depositphotos_214523660-stock-photo-two-young-technicians-mounting-heavy.jpg",
-    name:"lorem_214523660",
-    description:"lorem ipsum dolor sit amet, consectetur",
+    name:"18 inch Solar Standing Fan",
+    description:"18 inch Solar Standing Fan",
     price:""
   },
   {
     images:"https://st4.depositphotos.com/11085122/21452/i/450/depositphotos_214523660-stock-photo-two-young-technicians-mounting-heavy.jpg",
     name:"lorem_214523660",
-    description:"lorem ipsum dolor sit amet, consectetur",
+    description:"Beta Life Box",
     price:""
   },
   {
     images:"https://st4.depositphotos.com/11085122/21452/i/450/depositphotos_214523660-stock-photo-two-young-technicians-mounting-heavy.jpg",
     name:"lorem_214523660",
-    description:"lorem ipsum dolor sit amet, consectetur",
+    description:"Solar Mini Kit",
     price:""
   },
   {
     images:"https://st4.depositphotos.com/11085122/21452/i/450/depositphotos_214523660-stock-photo-two-young-technicians-mounting-heavy.jpg",
     name:"lorem_214523660",
-    description:"lorem ipsum dolor sit amet, consectetur",
+    description:"Desktop Solar Fan",
     price:""
   }
 
@@ -41,18 +41,7 @@ export default function Products() {
              <Product key={index} images={product.images} name={product.name} description={product.description} price={product.price}/>
             ))}
             </div>
-            <div className='Products'>
-           {products.map((product,index)=>(
-
-             <Product key={index} images={product.images} name={product.name} description={product.description} price={product.price}/>
-            ))}
-            </div>
-           <div className='Products'>
-           {products.map((product,index)=>(
-
-             <Product key={index} images={product.images} name={product.name} description={product.description} price={product.price}/>
-            ))}
-            </div>
+           
            
     </div>
   )
