ce9daccfa7908271ee2090a7503b066040414cdb
Geniuska275 <aigbojie2020@gmail.com>
2024-10-23 12:22:37 +0100

    updated

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { MdOutlineEnergySavingsLeaf } from "react-icons/md";
 function Service({text}) {
   return (
-    <div className='border border-black rounded p-[20px] w-[200px] h-[240px] mb-3 mr-3'>
+    <div className='border border-black rounded p-[20px] w-[300px] h-[240px] mb-3 mr-3'>
         <div className='rounded-full w-8 h-8 flex justify-center items-center bg-[#37CA2A]'>
         <MdOutlineEnergySavingsLeaf />
         </div>
