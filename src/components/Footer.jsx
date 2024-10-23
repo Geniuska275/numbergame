75cf4ccc48c5deabbb1317ff52781da73a017eee
Geniuska275 <aigbojie2020@gmail.com>
2024-10-23 18:27:20 +0100

    updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,25 +6,26 @@ import { FaInstagram } from "react-icons/fa";
 export default function Footer() {
   return (
     <div className='footer bg-[#EDF2EA]'>
-        <div className='Foot border-b p-2 push '>
-            <div className='space'>
-             <img src="" />
+        <div className='Foot border-b '>
+              <div className='space'>
+               <img src="" />
               <div>
-                <h1>Head Office :</h1>
+                <h1 className='font-extrabold font-[lato] mb-2'>Head Office</h1>
                 <p className='text-sm text-[#4A4E55]'>  3, Eguadase Street,<br></br> opposite James watt junction,
                 <br></br> off Akpakpava road,<br></br> Benin City, <br></br>Edo State.
                 </p>
               </div>
-            
             </div>
-            <div className='space mb-5'>
-                <h1>Warri Branch :</h1>
-                <p  className='text-sm text-[#4A4E55]'> 28, Effurum Sapele Road,<br></br> opposite unity bank,
+            <div className='space'>
+                <div>
+                <h1 className='font-extrabold font-[lato] mb-2'>Warri Branch</h1>
+                <p  className='text-sm text-[#4A4E55] mb-[20px]'> 28, Effurum Sapele Road,<br></br> opposite unity bank,
                 <br></br> before Enerhen junction ,<br></br>warri Delta State.
                 </p>
+                </div>
               </div>
             <div className='space' >
-                <h1> Company</h1>
+                <h1 className='font-extrabold font-[lato] mb-2'> Company</h1>
                 <a>
                     <h2 className='text-sm text-[#4A4E55] mb-2 hover:text-[#37CA2A]'>About us</h2>
                 </a>
@@ -40,9 +41,9 @@ export default function Footer() {
                 
             </div>
             <div className='space' >
-            <h1> Social Media</h1>
-            <div className='flex justify-between items-center'>
-           <FaFacebook className='text-2xl' />
+            <h1 className='font-extrabold font-[lato] mb-2'> Social Media</h1>
+            <div className='flex justify-between gap-4 items-center mb-[90px]'>
+            <FaFacebook className='text-2xl' />
             <BsTwitterX className='text-2xl' />
           <FaInstagram className='text-2xl' />
            <FaLinkedin  className='text-2xl'/>
