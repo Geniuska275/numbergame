1b9c47c1e06821109db1d8c8951606c185c251f7
Geniuska275 <aigbojie2020@gmail.com>
2024-10-23 08:33:31 +0100

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import {
  
 } from "react-router-dom";
 import Homepage from "./pages/Homepage";
+import { FaWhatsapp } from "react-icons/fa";
 import Navbar from "./components/Navbar";
 import Products from "./pages/Products";
 import Services from "./pages/Services";
@@ -30,8 +31,40 @@ function App() {
       <Route path="/Contact" element={<Contact/>} />
       
     </Routes>
+    <div style={{
+        position:"fixed",
+        bottom:"40px",
+        background:" #37CA2A",
+        width:"55px",
+        height:"55px",
+        borderRadius:"50%",
+        left:"20px",
+        display:"flex",
+        alignItems:"center",
+        justifyContent:"center",
+        zIndex:1000
+       
+
+      }}>
+
+         <div
+        style={{
+          width:"45px",
+        height:"45px",
+        borderRadius:"50%",
+        borderWidth:"2px",
+        display:"flex",
+        alignItems:"center",
+        justifyContent:"center"
+        }}
+        >
+        <a href="https://wa.me/+2347032188798">     
+        <FaWhatsapp color='white' size={25} />
+      </a>
+        </div>
+      </div>
     <Footer/>
-  
+    
   </Router>
 
    
