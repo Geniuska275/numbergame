f664cf11ced20a10945c656b0d86874dcfc3e3a1
Geniuska275 <aigbojie2020@gmail.com>
2024-10-25 08:06:59 +0100

    updated

--- src/components/Testimony.jsx
+++ src/components/Testimony.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Testimony({data}) {
   return (
-    <div className='shadow-lg rounded w-[300px] p-[20px] mb-3 mt-2'>
+    <div className='shadow-lg rounded w-[300px] h-[200px] p-[20px] mb-3 mt-2'>
         <h1 className='font-extrabold font-[lato]'>{data.product}</h1>
 
         <p className='font-bold font-[lato] text-[#4A4E55] m-auto'><i>
