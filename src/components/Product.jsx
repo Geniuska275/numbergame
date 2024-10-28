507fed7443eb976c402b32ccf78275f9d9d6729c
Geniuska275 <aigbojie2020@gmail.com>
2024-10-28 15:55:02 +0100

    updated

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -7,7 +7,7 @@ function Product({images, name, description}) {
         <h1 className='font-extrabold font-[lato]  text-2xl mb-2'> {description}</h1>
         <p className='font-bold font-[lato] text-[#4A4E55] text-sm mb-2'>{name}</p>
          
-        <a href='tel:+2348036808184'>
+        <a href='tel:+2348075362652'>
           <button className="bg-[#37CA2A]
           md: rounded-md w-full
           font-[lato] px-6 py-2 text-white shadow-lg hover:bg-[#37CA3A] duration-500">SHOP NOW</button>
