0f56a22fd8710d137e382f1be06dcf89bdcda719
Geniuska275 <aigbojie2020@gmail.com>
2024-10-28 11:15:52 +0100

    updated

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-
+import {motion} from "framer-motion"
 const styles={
   webkitLineClamp:4,
   webkitBoxOrient:"vertical",
@@ -22,11 +22,20 @@ export default function AboutUs() {
 
   return (
     <div className='About flex justify-around  p-[20px]'> 
-        <img src='https://media.istockphoto.com/id/1405880267/photo/two-engineers-installing-solar-panels-on-roof.jpg?s=612x612&w=0&k=20&c=OvQDbJaTnMM4jPfIA3y5vrO88i98NZJRahZtnYFZCq0='
+        <motion.img 
+                    whileInView={{opacity:1,x:0}}
+                    initial={{opacity:0,x:-100}}
+                    transition={{duration:1}} src='https://media.istockphoto.com/id/1405880267/photo/two-engineers-installing-solar-panels-on-roof.jpg?s=612x612&w=0&k=20&c=OvQDbJaTnMM4jPfIA3y5vrO88i98NZJRahZtnYFZCq0='
          className=' size w-[500px] h-[380px] rounded-lg shadow-lg'
         
         /> 
-        <div className={` sizes p-5 bg-[#f3fef9] rounded shadow `}>
+        <motion.div 
+                    whileInView={{opacity:1,x:0}}
+                    initial={{opacity:0,x:100}}
+                    transition={{duration:1}}
+
+          
+           className={` sizes p-5 bg-[#f3fef9] rounded shadow `}>
           <h1 className='text-2xl font-bold font-[lato] text-[#4A4E55] mb-2'>About Us</h1>
           <p className='text-sm'>
             {text}
@@ -35,7 +44,7 @@ export default function AboutUs() {
          <button className="bg-[#37CA3A] rounded-md 
            font-[lato] mt-2 px-6 py-2 text-black shadow-lg hover:bg-[#37CA3A] hover:text-white duration-500">{!isOpen ? "Read Less...":"Read More"}</button>
          </a>
-        </div>
+        </motion.div>
     </div>
   )
 }
